dc6ca4239299eb396cc8f0dcf2eda105122bd193
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2013-03-12 22:27:59 +0300

    add 3 labs

---
+++ 2_2.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+N = 1000
+# Матожидание
+def m(a)
+  a.inject(:+).to_f / N
+end
+
+# Бернулли
+def bernulli(p)
+  rand() < p ? 1 : 0
+end
+
+p = 0.7
+a = (0...N).to_a.map { bernulli(p) }
+p "Бернулли: #{a}"
+
+# Проверка
+mu = p
+d = p * (1-p)
+m = m(a)
+ksi1 = m - mu
+s2 = a.map { |ai| (ai - m) ** 2 }.inject(:+) / (N - 1)
+ksi2 = s2 - d
+
+puts "ksi1: #{ksi1}, ksi2: #{ksi2}"
+
+# Биномиальное
+def binomial(m, p)
+  a = (0...m).to_a.map { rand() }
+  x = a.map{ |ai| l(p - ai) }.inject(:+)
+end
+
+def l(z)
+  z <= 0 ? 0 : 1
+end
+
+m, p = 5, 0.25
+a = (0...N).to_a.map { binomial(m, p) }
+p "Биномиальное: #{a}"
+
+# Проверка
+mu = m * p
+d = m * p * (1-p)
+m = m(a)
+ksi1 = m - mu
+s2 = a.map { |ai| (ai - m) ** 2 }.inject(:+) / (N - 1)
+ksi2 = s2 - d
+
+puts "ksi1: #{ksi1}, ksi2: #{ksi2}"
+
+# Однородная цепь Маркова
+S = [ 0, 1, 2 ]
+T = 1000
+Pi = [ 0.33333, 0.33333, 0.33334 ]
+P = [ [ 0.1, 0.2, 0.7 ], [ 0.3, 0.4, 0.3 ], [ 0.6, 0.2, 0.2 ] ]
+
+def markov(p)
+  q = [ 0, p[0], p[0..1].inject(:+), p[0..2].inject(:+) ]
+  a = rand()
+  (1..3).each { |i| return S[i-1] if (q[i-1]...q[i]).member?(a) }
+end
+
+def count(a)
+  h = Hash.new
+  a.uniq.each { |e| h[e] = a.count(e).to_f / a.count }
+  h
+end
+
+p "Markov Pi: #{count( (0...T).to_a.map { markov(Pi) } )}"
+
+a = [ markov(Pi) ]
+(1...T).each { |i| a << markov(P[S.index(a[i-1])]) }
+puts "Вывести средние доли нулей, единиц и двоек в n смоделированных последовательностях."
+p count(a)
---
+++ 3_2.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+N = 1000
+# Матожидание
+def me(a)
+  a.reduce(:+) / a.size
+end
+# Проверка
+def check(a, mu, d)
+  m = me(a)
+  ksi1 = m - mu
+  s2 = a.map { |ai| (ai - m) ** 2 }.reduce(:+).to_f / (a.size - 1)
+  ksi2 = s2 - d
+  "ksi1: #{ksi1}, ksi2: #{ksi2}"
+end
+
+# N(m, s2)
+def standard_normal(n = 48)
+  Math.sqrt(12.to_f/n) * ((1..n).map { rand }.reduce(:+) - n/2)
+end
+
+m = 1
+s2 = 9
+
+def standard(normal, m, s2)
+  normal*Math.sqrt(s2) + m
+end
+a = (1..N).map { standard(standard_normal, m, s2) }
+p "N(#{m}, #{s2}): #{a}"
+puts "N(#{m}, #{s2}) проверка: #{check(a, m, s2)}"
+
+# χ 2 (m)
+def xi2(m)
+  (1..m).map { standard_normal ** 2 }.reduce(:+)
+end
+
+m = 4
+mu1 = m
+d1 = 2*m
+
+a = (1..N).map { xi2(m) }
+p "Xi^2, m=#{m}: #{a}"
+puts "Xi^2, m=#{m} проверка: #{check(a, mu1, d1)}"
+
+# Стьюдента (t-распределение) t(m)
+m = 6
+mu2 = 0
+d2 = m.to_f/(m-2)
+
+def student(m)
+  (standard_normal).to_f / Math.sqrt(xi2(m).to_f / m)
+end
+
+a = (1..N).map { student(m) }
+p "Стьюдента, m=#{m}: #{a}"
+puts "Стьюдента, m=#{m} проверка: #{check(a, mu2, d2)}"
+
+# из смеси двух распределений
+Pi = 0.3
+
+a = (1..N).map { rand < 1 - Pi ? standard(standard_normal, mu1, d1) : standard(standard_normal, mu2, d2) }
+p "Cмеси: #{a}"
+# puts "\nCмеси проверка: #{check(a, mu2, d2)}"
